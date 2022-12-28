3ef45fa5da16291dea83cc45ac4afbe8b55b74c3
Hardik Modha <hardik@technopers.com>
2022-12-28 13:12:20 +0530

    feat: added multiple relationships to HasTrackedJobs.php

--- src/Concerns/HasTrackedJobs.php
+++ src/Concerns/HasTrackedJobs.php
@@ -4,11 +4,36 @@ namespace Technopers\LaravelTrackableJobs\Concerns;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Technopers\LaravelTrackableJobs\Models\TrackedJob;
+use Technopers\LaravelTrackableJobs\TrackedJobStatuses;
 
 trait HasTrackedJobs
 {
     public function trackedJobs(): MorphMany
     {
         return $this->morphMany(TrackedJob::class, 'trackable');
+    }
+
+    public function getFinishedJobs(): MorphMany
+    {
+        return $this->morphMany(TrackedJob::class, 'trackable')
+            ->where('status', TrackedJobStatuses::STATUS_FINISHED);
+    }
+
+    public function getRunningJobs(): MorphMany
+    {
+        return $this->morphMany(TrackedJob::class, 'trackable')
+            ->where('status', TrackedJobStatuses::STATUS_STARTED);
+    }
+
+    public function getPendingJobs(): MorphMany
+    {
+        return $this->morphMany(TrackedJob::class, 'trackable')
+            ->where('status', TrackedJobStatuses::STATUS_QUEUED);
+    }
+
+    public function getFailedJobs(): MorphMany
+    {
+        return $this->morphMany(TrackedJob::class, 'trackable')
+            ->where('status', TrackedJobStatuses::STATUS_FAILED);
     }
 }
